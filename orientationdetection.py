54cdb69c7f9b5efb1e5f188755ab1d4732dc9907
LFC6times <72238524+LFC6times@users.noreply.github.com>
2023-01-31 16:25:14 -0800

    yay it work i guess

--- orientationdetection.py
+++ orientationdetection.py
@@ -3,64 +3,6 @@ from math import atan2, cos, sin, sqrt, pi
 import numpy as np
  
 DELTAVALUE = 20 # variation for how wide the angle "upright" can be, in each direction
-def drawAxis(img, p_, q_, color, scale):
-  p = list(p_)
-  q = list(q_)
- 
-  ## [visualization1]
-  angle = atan2(p[1] - q[1], p[0] - q[0]) # angle in radians
-  hypotenuse = sqrt((p[1] - q[1]) * (p[1] - q[1]) + (p[0] - q[0]) * (p[0] - q[0]))
- 
-  # Here we lengthen the arrow by a factor of scale
-  #q[0] = p[0] - scale * hypotenuse * cos(angle)
-  #q[1] = p[1] - scale * hypotenuse * sin(angle)
-  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
- 
-  # create the arrow hooks
-  p[0] = q[0] + 9 * cos(angle + pi / 4)
-  p[1] = q[1] + 9 * sin(angle + pi / 4)
-  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
- 
-  p[0] = q[0] + 9 * cos(angle - pi / 4)
-  p[1] = q[1] + 9 * sin(angle - pi / 4)
-  cv.line(img, (int(p[0]), int(p[1])), (int(q[0]), int(q[1])), color, 3, cv.LINE_AA)
-  ## [visualization1]
- 
-def getOrientation(pts, img):
-  ## [pca]
-  # Construct a buffer used by the pca analysis
-  sz = len(pts)
-  data_pts = np.empty((sz, 2), dtype=np.float64)
-  for i in range(data_pts.shape[0]):
-    data_pts[i,0] = pts[i,0,0]
-    data_pts[i,1] = pts[i,0,1]
- 
-  # Perform PCA analysis
-  mean = np.empty((0))
-  mean, eigenvectors, eigenvalues = cv.PCACompute2(data_pts, mean)
- 
-  # Store the center of the object
-  max_area_contourr = (int(mean[0,0]), int(mean[0,1]))
-  ## [pca]
- 
-  ## [visualization]
-  # Draw the principal components
-  cv.circle(img, max_area_contourr, 3, (255, 0, 255), 2)
-  p1 = (max_area_contourr[0] + 0.02 * eigenvectors[0,0] * eigenvalues[0,0], max_area_contourr[1] + 0.02 * eigenvectors[0,1] * eigenvalues[0,0]) # ref line
-  p2 = (max_area_contourr[0] - 0.02 * eigenvectors[1,0] * eigenvalues[1,0], max_area_contourr[1] - 0.02 * eigenvectors[1,1] * eigenvalues[1,0]) # the angle of the thingy
-  drawAxis(img, max_area_contourr, p1, (255, 255, 0), 1)
-  drawAxis(img, max_area_contourr, p2, (0, 0, 255), 5)
- 
-  angle = atan2(eigenvectors[0,1], eigenvectors[0,0]) # orientation in radians
-  ## [visualization]
- 
-  # Label with the rotation angle
-  # label = "  Rotation Angle: " + str(-int(np.rad2deg(angle)) - 90) + " degrees"
-  label = "  Rotation Angle: " + str(int(np.rad2deg(angle))) + " degrees"
-  textbox = cv.rectangle(img, (max_area_contourr[0], max_area_contourr[1]-25), (max_area_contourr[0] + 250, max_area_contourr[1] + 10), (255,255,255), -1)
-  cv.putText(img, label, (max_area_contourr[0], max_area_contourr[1]), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv.LINE_AA)
- 
-  return angle
  
 def returnOrientation(angle): # return true if angle is within upright range
     print(angle)
@@ -111,7 +53,7 @@ while True:
             max_area_contour = c
         
         # Ignore contours that are too small or too large
-        if area < 1000 or 100000 < area:
+        if area < 1000 or 300000 < area:
             continue
         
         #india
@@ -148,17 +90,14 @@ while True:
         count = 0
         if leftmost[1] > cY:
           count += 1
-        if rightmost[1] > cy:
+        if rightmost[1] > cY:
           count += 1
         if topmost[1] > cY:
           count += 1
         if bottommost[1] > cY:
           count += 1
-
-        if previous_angle is not None:
-            continue
         
-        if angle - previous_angle > 90: # jumping from 0 to 180 degrees
+        if previous_angle is None or angle - previous_angle > 90: # jumping from 0 to 180 degrees
             if count > 2: # cone tip pointing down
                 invert_angle = False
             else: # cone tip pointing up
@@ -172,9 +111,9 @@ while True:
         previous_angle = angle
         
         if invert_angle:
-            print (angle + 180)
-        else:
             print (angle)
+        else:
+            print (angle + 180)
         
         # Find the orientation of each shape
         #angle = getOrientation(max_area_contour, img)
