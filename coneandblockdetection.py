e46a2c33510f9fa9b85d3183d3f2139ecfa7893a
Sean Son <astroson2005@gmail.com>
2023-02-04 10:41:09 -0800

    tuned

--- coneandblockdetection.py
+++ coneandblockdetection.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import time
 
-cube = True
+cube = False 
 cap = cv2.VideoCapture(1+cv2.CAP_DSHOW)
 
 x_res = 720 #determine later
--- orientationdetection.py
+++ orientationdetection.py
@@ -13,10 +13,10 @@ def returnOrientation(angle): # return true if angle is within upright range
         return False
         # return False
 
-cap = cv.VideoCapture(0 + cv.CAP_DSHOW)
+cap = cv.VideoCapture(1 + cv.CAP_DSHOW)
  
-lower_threshold = np.array([18, 44, 101])   # determined experimentally
-upper_threshold = np.array([31, 232, 255])   # determined experimentally
+lower_threshold = np.array([18, 44, 170])   # determined experimentally
+upper_threshold = np.array([31, 255, 255])   # determined experimentally
 
 invert_angle = False
 previous_angle = None
@@ -37,75 +37,73 @@ while True:
 
     # convert the hsv image to binary image + noise reduction
     thresh = cv.inRange(hsv, lower_threshold, upper_threshold)
-    noise_reduction = cv.blur(thresh,(20,20))
-    noise_reduction = cv.inRange(noise_reduction, 1, 75)
     noise_reduction = cv.erode(thresh, np.ones((10, 10), np.uint8), iterations = 1)
+    noise_reduction = cv.blur(thresh,(15,15))
+    noise_reduction = cv.inRange(noise_reduction, 169, 255)
     
     # Find all the contours in the thresholded image
     contours, _ = cv.findContours(noise_reduction, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 
     if not len(contours) == 0:
         max_area_contour = contours[0]
+        for i, c in enumerate(contours):
+            area = cv.contourArea(c)
+            # Ignore contours that are too small or too large
+            if area < 1000 or 300000 < area:
+                continue
 
-    for i, c in enumerate(contours):
-        area = cv.contourArea(c)
-        if area > cv.contourArea(max_area_contour):
-            max_area_contour = c
-        
-        # Ignore contours that are too small or too large
-        if area < 1000 or 300000 < area:
-            continue
-        
-        #india
+            if area > cv.contourArea(max_area_contour):
+                max_area_contour = c
 
-        M = cv.moments(max_area_contour)
-        cX = int(M["m10"] / M["m00"])
-        cY = int(M["m01"] / M["m00"])
-        # draw the contour and center of the shape on the image
-        cv.circle(img, (cX, cY), 7, (0, 0, 0), -1)
+        #if not (cv.contourArea(max_area_contour) < 2000): #if there was no larger contour, must check the initial one
+            M = cv.moments(max_area_contour)
+            if not M['m00'] == 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+                # draw the contour and center of the shape on the image
+                cv.circle(img, (cX, cY), 7, (0, 0, 0), -1)
 
-        leftmost = tuple(max_area_contour[max_area_contour[:,:,0].argmin()][0])
-        rightmost = tuple(max_area_contour[max_area_contour[:,:,0].argmax()][0])
-        topmost = tuple(max_area_contour[max_area_contour[:,:,1].argmin()][0])
-        bottommost = tuple(max_area_contour[max_area_contour[:,:,1].argmax()][0])
+                leftmost = tuple(max_area_contour[max_area_contour[:,:,0].argmin()][0])
+                rightmost = tuple(max_area_contour[max_area_contour[:,:,0].argmax()][0])
+                topmost = tuple(max_area_contour[max_area_contour[:,:,1].argmin()][0])
+                bottommost = tuple(max_area_contour[max_area_contour[:,:,1].argmax()][0])
 
-        cv.circle(img, leftmost, 5, (0, 0, 0), 2)
-        cv.circle(img, rightmost, 5, (0, 0, 0), 2)
-        cv.circle(img, topmost, 5, (0, 0, 0), 2)
-        cv.circle(img, bottommost, 5, (0, 0, 0), 2)
+                cv.circle(img, leftmost, 5, (0, 0, 0), 2)
+                cv.circle(img, rightmost, 5, (0, 0, 0), 2)
+                cv.circle(img, topmost, 5, (0, 0, 0), 2)
+                cv.circle(img, bottommost, 5, (0, 0, 0), 2)
 
-        # Draw each contour only for visualisation purposes
-        cv.drawContours(img, contours, i, (0, 0, 255), 2)
+                # Draw each contour only for visualisation purposes
+                cv.drawContours(img, contours, i, (0, 0, 255), 2)
 
-        (x,y),(MA,ma),angle = cv.fitEllipse(max_area_contour)
+                (x,y),(MA,ma),angle = cv.fitEllipse(max_area_contour)
 
-        count = 0
-        if leftmost[1] > cY:
-          count += 1
-        if rightmost[1] > cY:
-          count += 1
-        if topmost[1] > cY:
-          count += 1
-        if bottommost[1] > cY:
-          count += 1
-        
-        if previous_angle is None or angle - previous_angle > 90: # jumping from 0 to 180 degrees
-            if count > 2: # cone tip pointing down
-                invert_angle = True
-            else: # cone tip pointing up
-                invert_angle = False
-        elif angle - previous_angle < -90: # jumping from 180 to 0 degrees
-            if count > 2: # cone tip pointing down
-                invert_angle = False
-            else: # cone tip pointing up
-                invert_angle = True
+                count = 0
+                if leftmost[1] > cY:
+                    count += 1
+                if rightmost[1] > cY:
+                    count += 1
+                if topmost[1] > cY:
+                    count += 1
+                if bottommost[1] > cY:
+                    count += 1
 
-        previous_angle = angle
-        
-        if invert_angle:
-            angle += 180
-        
-        print (angle)
+                if previous_angle is None or angle - previous_angle > 90: # jumping from 0 to 180 degrees
+                    if count > 2: # cone tip pointing down
+                        invert_angle = True
+                    else: # cone tip pointing up
+                        invert_angle = False
+                elif angle - previous_angle < -90: # jumping from 180 to 0 degrees
+                    if count > 2: # cone tip pointing down
+                        invert_angle = False
+                    else: # cone tip pointing up
+                        invert_angle = True
+
+                previous_angle = angle
+
+                if invert_angle:
+                    angle += 180
+                print (angle)
 
     cv.imshow('Output Image', img)
     cv.imshow('Noise Reduction', noise_reduction)
