0421fb687cce19cf50d4888dc49d5a9738fcc21b
siIverfish <ivan.g.zimmerman@gmail.com>
2023-02-12 16:01:00 -0800

    objectdetection is great, but I need to revert the other files.

---
+++ contour_lib.py
@@ -0,0 +1,53 @@
+""" Useful functions for working with points and contours. """
+
+import numpy as np
+import cv2 as cv
+
+
+def get_maximum_contour(contours):
+    """ Returns the contour with the largest area in the list of contours or None if there are no contours. """
+    contours = [
+        contour for contour in contours \
+        if 1_000 < cv.contourArea(contour) < 300_000
+    ]
+    if len(contours) == 0:
+        return None
+    return max(contours, key=cv.contourArea)
+
+
+def get_contour_center(contour):
+    """ Returns the center of the contour. """
+    moments = cv.moments(contour)
+    # No one knows what this does, but it works.
+    center_x = int(moments["m10"] / moments["m00"])
+    center_y = int(moments["m01"] / moments["m00"])
+    return np.array([center_x, center_y])
+
+
+def distance_squared(point1, point2):
+    """Returns the distance between two points, but squared for performance because we only need the distance for comparison."""
+    return (point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2
+
+
+def get_angle(point1, point2):
+    """Returns the angle from the first point to the second point in degrees."""
+    degrees = np.degrees(np.arctan2(point2[1] - point1[1], point2[0] - point1[0]))
+    while degrees < 0:
+        degrees += 360
+    return -degrees + 360
+
+
+def get_farthest_point(contour, center=None):
+    """Returns the farthest point from the center of the contour."""
+    if center is None:
+        center = get_contour_center(contour)
+    return max(
+        (point[0] for point in contour),
+        key=lambda point: distance_squared(center, point),
+    )
+
+
+def get_orientation(contour, center=None):
+    """Returns the angle of the farthest point from the center of the contour."""
+    farthest_point = get_farthest_point(contour, center)
+    return angle(center, farthest_point)
---
+++ objectdetection.py
@@ -0,0 +1,119 @@
+""" 
+Detects objects in the camera stream. 
+The main loop could work better, but I don't want to work on finding another threshold.
+
+BTW: I made a docstring for all of the functions, so you can hover over them to see what they do.
+"""
+
+from dataclasses import dataclass
+
+import numpy as np
+import cv2 as cv
+
+from fps_counter import FPSCounter
+from contour_lib import (
+    get_maximum_contour,
+    get_contour_center,
+    get_farthest_point,
+    get_angle,
+)
+
+
+@dataclass
+class Threshold:
+    """
+    Holds the lower and upper threshold color values for the objects.
+    I like it this way but we could replace it with just passing static values to `process_object`.
+    """
+
+    lower: np.ndarray
+    upper: np.ndarray
+
+
+def infinite_frame_stream():
+    """
+    Returns a generator that yields frames from the webcam.
+    Also exits the program if the user presses 'q' and waits between frames.
+    """
+    cap = cv.VideoCapture(0)
+    while True:
+        _, frame = cap.read()
+        if frame is None:
+            print("Error reading frame")
+            exit(1)
+        # Exit if the user presses 'q'
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            exit(0)
+        yield frame
+
+
+def process_object(threshold):
+    """
+    Given a threshold, this function will process the camera stream
+    and detect the object of interest.
+
+    Args:
+        threshold (Threshold): An object with `lower` and `upper` attributes, which are HSV values.
+    """
+    fps_counter = FPSCounter()
+
+    for frame in infinite_frame_stream():
+        # Blurs the image to reduce noise
+        processed = cv.medianBlur(frame, 21)
+
+        # Converts the image to the Hue, Saturation, Value color space, which makes it easier to detect objects of a certain color
+        processed = cv.cvtColor(processed, cv.COLOR_BGR2HSV)
+
+        # Converts the image to a binary image, where the object of interest (between the lower and upper thresholds) is white and the rest is black
+        processed = cv.inRange(processed, threshold.lower, threshold.upper)
+
+        # Finds objects in the new binary image, which should be easy because the image is only black and white.
+        contours, _ = cv.findContours(processed, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        # Gets the largest contour in the image, hopefully the correct object if the threshold is set well.
+        largest_object = get_maximum_contour(contours)
+
+        # Gets the center of the largest contour
+        object_center = get_contour_center(largest_object)
+        farthest_point = get_farthest_point(largest_object, object_center)
+
+        object_angle = get_angle(object_center, farthest_point)
+
+        cv.circle(frame, object_center, 5, (0, 255, 0), -1)
+        cv.circle(frame, farthest_point, 5, (0, 0, 255), -1)
+        cv.line(frame, object_center, farthest_point, (255, 0, 0), 2)
+        show_text(frame, f"Angle: {object_angle:.1f}")
+
+        cv.imshow("frame", frame)
+        fps_counter.count()
+
+
+def show_text(frame, text):
+    """ 
+    Shows text on the top-left of the frame. Made another function because this looks bad in the main loop. 
+    
+    Args:
+        frame (np.ndarray): The frame to show the text on.
+        text (str): The text to show.
+    """
+    cv.putText(
+        frame,
+        text,
+        (10, 30),
+        cv.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255, 255, 255),
+        2,
+    )
+
+
+def main():
+    """The main function. Detects the printed cone in the camera stream."""
+    printed_cone_threshold = Threshold(
+        lower=np.array([8, 44, 101]), upper=np.array([21, 232, 255])
+    )
+    process_object(threshold=printed_cone_threshold)
+
+
+if __name__ == "__main__":
+    main()
